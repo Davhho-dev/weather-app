4393a73bf6525324b42ba08847c7502ec82fbb6e
Davhho-dev <Davhho@utexas.edu>
2023-03-16 23:08:25 -0500

    Import search module and add event listener for search/right arrow icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { moveSlider } from "./slider";
+import { searchBar } from "./search";
 
 const sliders = document.querySelector(".switch");
 sliders.addEventListener("click", (e) => {
     moveSlider();
+});
+
+const form = document.querySelector("form");
+form.addEventListener("click", (e) => {
+    if(e.target.id === "search-icon" || e.target.id === "right-arrow") searchBar(); 
 });
