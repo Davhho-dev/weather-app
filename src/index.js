a67823f7ae58809e392f70fed9a14954035cdc95
Davhho-dev <Davhho@utexas.edu>
2023-03-20 20:36:25 -0500

    Import search/UI modules, revise slider and search event listener

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import { moveSlider } from "./slider";
-import { animateSearchBar } from "./search";
+import { animateSearchBar, searchBar } from "./search";
+import { updateLocation } from "./UI";
+
+let units = "imperial";
+let inputArr = [];
 
 const sliders = document.querySelector(".switch");
 sliders.addEventListener("click", (e) => {
-    moveSlider();
+  units = moveSlider();
+  if(inputArr.length > 0) searchBar(inputArr, units);
 });
 
 const form = document.querySelector("form");
 form.addEventListener("click", (e) => {
-    if(e.target.id === "search-icon" || e.target.id === "right-arrow") animateSearchBar(); 
+  if (e.target.id === "search-icon") animateSearchBar();
+  else if (e.target.id === "right-arrow") {
+    e.preventDefault();
+    animateSearchBar();
+    const userInput = document.getElementById("search").value;
+    if (userInput.indexOf(",") >= 0)
+      inputArr = userInput.split(", "); //check for whitespace
+    else inputArr = userInput.split(",");
+    searchBar(inputArr, units);
+    updateLocation(inputArr);
+  }
 });
